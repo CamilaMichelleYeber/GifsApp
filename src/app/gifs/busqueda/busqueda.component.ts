17b728d6a9ade381eab252ff330db23a310c463d
CamilaMichelleYeber <camilamichelleyeber@outlook.com>
2021-08-10 15:46:52 -0300

    Utilizacion de @ViewChild en mi componente de busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
 
-  constructor() { }
+  @ViewChild('txtBusqueda') txtBusqueda!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  buscar(){
+    //mostramos los valores en consola
+    const valor =this.txtBusqueda.nativeElement.value;
+    console.log(valor);
+
+    //limpiamos la caja de busqueda
+    this.txtBusqueda.nativeElement.value= "";
   }
-
 }
