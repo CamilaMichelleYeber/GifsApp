b16f935a84094813de3005e72ca29def6eacf1ca
CamilaMichelleYeber <camilamichelleyeber@outlook.com>
2021-08-12 12:01:17 -0300

    Peticion HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   //propiedad que obtiene los valores que en tiempo real se van cambiando
+  
+  private apiKey: string= 'xi9TrWCu2nVoTY6NVdBmHBF5RGO6s1AZ';
   private _historial: string[]=[];
+
+  //creamos contructor para importar mi HttpClientModule
+  constructor ( private http: HttpClient){}
 
   
   get historial(){
@@ -21,8 +27,14 @@ export class GifsService {
       this._historial.unshift(historialBusqueda); //el ultimo dato que ponemos en la caja de busqueda se coloca primero en mi panel lateral
     this._historial= this._historial.splice(0,10); //con esto cortamos nuestro array principal limitandolo de 0 a 10    
     }
-    
     console.log(this._historial);
+
+    //utilizaremos observable para llamar a mi propiedad http y hacer la peticion
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=xi9TrWCu2nVoTY6NVdBmHBF5RGO6s1AZ&limit=10')
+      .subscribe( (respuesta:any) => {
+        console.log(respuesta.data);
+      })
+
   }
   
 }
